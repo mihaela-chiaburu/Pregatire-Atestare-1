4c72cbb27af7bd106940cf65abd72e14ad52aded
Mihaela C <mchiaburu.1337@gmail.com>
2023-10-20 23:38:36 +0300

    Update project

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,43 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+/*Encapsulation in OOP means bundling data and methods into a class, and controlling access
+to the data through public methods. It hides the internal details of the class and provides
+a secure and organized way to interact with objects.*/
+
 public class Main {
     public static void main(String[] args){
-        //------------------Conditions and Operators and Loops:
-        //1. Write a Java program to determine if a given number is even or odd.
         Scanner scanner = new Scanner(System.in);
+        //------------------Conditions and Operators and Loops:
+        Ex1();
+        Ex2();
+        Ex3();
+        //------------------Arrays:
+        Ex4();
+        Ex5();
+        Ex6();
+        //------------------Exceptions:
+        Ex7();
+        //------------------Lists:
+        Ex8();
+        Ex9();
+
+        //------------------OOP
+
+    }
+
+    public static void Ex1(){
+        Scanner scanner = new Scanner(System.in);
+        //1. Write a Java program to determine if a given number is even or odd.
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
         if(number%2==1)
             System.out.println("Odd number");
         else
             System.out.println("Even number");
-
+    }
+    public static void Ex2(){
+        Scanner scanner = new Scanner(System.in);
         //2. Create a program that takes two numbers as input and finds the larger of the two.
         System.out.print("Enter two numbers: ");
         int n1 = scanner.nextInt();
@@ -24,7 +49,9 @@ public class Main {
             System.out.println("Numbers are equal");
         else
             System.out.println(n2 + " is larger");
-
+    }
+    public static void Ex3(){
+        Scanner scanner = new Scanner(System.in);
         //3. Implement a simple calculator that can perform addition, subtraction, multiplication, and division
         int loop = 1;
         while(loop==1){
@@ -60,8 +87,9 @@ public class Main {
             if (loop == 0)
                 break;
         }
-
-        //-------------------------- Arrays:
+    }
+    public static void Ex4(){
+        Scanner scanner = new Scanner(System.in);
         //1. Find the sum of all elements in an integer array.
         int[] arr = new int[100];
         System.out.print("Enter the nr of elements in the array: ");
@@ -73,16 +101,19 @@ public class Main {
             sum +=arr[i];
         }
         System.out.println("The sum of all the elements: " + sum);
-
+    }
+    public static void Ex5(){
+        Scanner scanner = new Scanner(System.in);
         //2. Calculate the average of elements in a double array.
         double[] arr2 = {5.2, 4.6, 10.8, 7.6, 11.333, 7.0};
         double avg, sumD = 0;
         for (double v : arr2) {
             sumD += v;
         }
-
         System.out.println("\nThe average in the array {5.2, 4.6, 10.8, 7.6, 11.333, 7.0} is: " + sumD/ arr2.length);
-
+    }
+    public static void Ex6(){
+        Scanner scanner = new Scanner(System.in);
         //3. Find the maximum and minimum values in an array.
         int[] arr3 = new int[25];
         System.out.print("Enter the nr of elements in the array: ");
@@ -91,17 +122,17 @@ public class Main {
         arr3[0] = scanner.nextInt();
         int Max = arr3[0];
         int Min = arr3[0];
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n3; i++){
             arr3[i] = scanner.nextInt();
-            if(arr[i]>Max)
+            if(arr3[i]>Max)
                 Max = arr3[i];
             if(arr3[i]<Min)
                 Min = arr3[i];
         }
         System.out.println("Max element: " + Max);
         System.out.println("Min element: " + Min);
-
-        //---------------------Exceptions:
+    }
+    public static void Ex7(){
         try {
             // Code that may throw an exception
             int numerator = 10;
@@ -113,8 +144,8 @@ public class Main {
             System.err.println("An error occurred: " + e.getMessage());
         }
         System.out.println("Program continues after the try-catch block.");
-
-        //-------------------Lists:
+    }
+    public static void Ex8(){
         //1. Create an ArrayList of integers, add elements to it, and find the sum of all elements.
         ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(5);
@@ -126,8 +157,9 @@ public class Main {
             suma += e;
         }
         System.out.println("ArrayList elements: " + numbers);
-        System.out.println("Sum of elements: " + sum);
-
+        System.out.println("Sum of elements: " + suma);
+    }
+    public static void Ex9(){
         //2. Create a List<String, add elements to it, and find the sum of all the elements' lengths
         List<String> words = new ArrayList<>();
         words.add("apple");
